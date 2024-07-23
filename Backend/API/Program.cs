fd4c6859ff2f75efa9b08dbf930652425bf7e7a6
Luka <luleee@elfak.rs>
2024-07-23 15:40:56 +0200

    Install packages

--- Backend/API/Program.cs
+++ Backend/API/Program.cs
@@ -1,25 +1,48 @@
-var builder = WebApplication.CreateBuilder(args);
+// var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// // Add services to the container.
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+// builder.Services.AddControllers();
+// // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+// builder.Services.AddEndpointsApiExplorer();
+// builder.Services.AddSwaggerGen();
 
-var app = builder.Build();
+// var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
+// // Configure the HTTP request pipeline.
+// if (app.Environment.IsDevelopment())
+// {
+//     app.UseSwagger();
+//     app.UseSwaggerUI();
+// }
+
+// app.UseHttpsRedirection();
+
+// app.UseAuthorization();
+
+// app.MapControllers();
+
+// app.Run();
+
+
+using DiffMatchPatch;
+
+diff_match_patch dmp = new();
+
+string str1 = "Hello World";
+string str2 = "Hello World but in Mexican";
+
+List<Diff> diffs = dmp.diff_main(str1, str2);
+
+List<Patch> patch = dmp.patch_make(diffs);
+
+// foreach (var v in diffs) {
+//     Console.WriteLine(v);
+// }
+
+foreach (var p in patch) {
+    Console.WriteLine(p);
 }
 
-app.UseHttpsRedirection();
+var obj = dmp.patch_apply(patch, str1);
 
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
